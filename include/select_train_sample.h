e96645601e8271439ecee786f0fe0890d3dd4091
binLearning <gxbgrx@sina.com>
2017-06-17 00:04:57 +0800

    add select_train_sample.h

---
+++ include/select_train_sample.h
@@ -0,0 +1,12 @@
+/*
+ * Select training sample
+ */
+
+#ifndef _SELECT_TRAIN_SAMPLE_H_
+#define _SELECT_TRAIN_SAMPLE_H_
+
+#include <string>
+
+void SelectTrainSample(std::string dir_src);
+
+#endif
